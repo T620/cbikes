ab6d2201b49b421e945d4726039f05a5da399d3d
Josh Tait <Josh.tait3@gmail.com>
2017-06-06 20:18:07 +0100

    setup db

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = "\xb2\xc3\xa5$O,FJx\xa3\x14vB(\xc5\xe3\xa3\x1a)\xa4\x81\xf2\x8bK"
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SECRET_KEY = "\xb2\xc3\xa5$O,FJx\xa3\x14vB(\xc5\xe3\xa3\x1a)\xa4\x81\xf2\x8bK"
 
 
 class ProductionConfig(Config):
